69750fbf473c269244348553f4727cd9a1e62681
Eiji Iwazawa <e-iwazawa@mapion.co.jp>
2012-08-27 19:01:28 +0900

    ignore no port host

--- lib/moped/node.rb
+++ lib/moped/node.rb
@@ -375,7 +375,7 @@ module Moped
       peers.concat(info["passives"]) if info["passives"]
       peers.concat(info["arbiters"]) if info["arbiters"]
 
-      @peers = peers.map { |peer| Node.new(peer) }
+      @peers = peers.select {|peer| peer.include?(':')}.map { |peer| Node.new(peer) }
       @primary, @secondary = primary, secondary
       @arbiter = info["arbiterOnly"]
       @passive = info["passive"]
